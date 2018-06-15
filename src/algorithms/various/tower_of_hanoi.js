ca93627373e96a030ce15e1edde93035a01fdb2f
Antony Baasandorj <antony.baasan@gmail.com>
2018-06-15 14:26:36 -0400

    remove console

--- src/algorithms/various/tower_of_hanoi.js
+++ src/algorithms/various/tower_of_hanoi.js
@@ -8,14 +8,12 @@ function toh(n, fromStack, toStack, tempStack) {
   if (n == 1) {
     var a = fromStack.pop();
     toStack.push(a);
-    console.log("move", a);
     return;
   }
 
   toh(n - 1, fromStack, tempStack, toStack);
   var b = fromStack.pop();
   toStack.push(b);
-  console.log("move2", b);
   toh(n - 1, tempStack, toStack, fromStack);
 }
 
